f4b3579295ef6d21a54041a726577d068c33d000
Lucas.Goh <leftcompact@gmail.com>
2022-03-13 20:18:08 +0900

    add gif

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 
 # BFS Path Finding Visualization 
 
-! [](shorts.gif)
+![shorts](https://user-images.githubusercontent.com/28504486/158056964-762e82b1-f2d7-46d8-98a1-5a7812e2f37d.gif)
--- src/bfsSearch/GameFrame.java
+++ src/bfsSearch/GameFrame.java
@@ -6,7 +6,7 @@ import javax.swing.JFrame;
 public class GameFrame extends JFrame{
 	GameFrame(){
 		this.add(new GamePanel());
-		this.setTitle("snake");
+		this.setTitle("BFS path finding");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 		this.pack();
